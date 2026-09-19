64365eb199d021aacccdaadcac20f2327ad2cc23
Muhmmad Abid Azhar <abidazhar006@gmail.com>
2026-09-19 18:40:11 +0700

    chore: restrukturisasi folder untuk Expo Router

--- src/app/tabs/index.tsx
+++ src/app/tabs/index.tsx
@@ -1,9 +1,9 @@
 import { View } from 'react-native';
 import { useState, useEffect } from 'react';
-import WeatherCard from '../components/WeatherCard';
-import SearchBox from '../components/SearchBox';
-import RiwayatList from '../components/RiwayatList';
-import IndikatorAQI from "../components/IndikatorAQI"; // Latihan Mandiri - Pertemuan 2
+import WeatherCard from '../../components/WeatherCard';
+import SearchBox from '../../components/SearchBox';
+import RiwayatList from '../../components/RiwayatList';
+import IndikatorAQI from "../../components/IndikatorAQI"; // Latihan Mandiri - Pertemuan 2
 
 export default function HalamanUtama() {
   const [kotaAktif, setKotaAktif] = useState("Pekalongan");
